8bfafef7edadc62d1b74a38599b2da781c40901a
Luuk Wubben <l.t.wubben@student.tudelft.nl>
2021-11-25 10:00:58 +0100

    Add User class to main gateway

---
+++ main_gateway/src/main/java/nl/tudelft/sem/template/Objects/User.java
@@ -0,0 +1,57 @@
+package nl.tudelft.sem.template.Objects;
+
+import java.util.Objects;
+
+public class User {
+
+    private final String id;
+    private final String firstName;
+    private final String lastName;
+    private final String userType;
+
+    public User(String id, String firstName, String lastName, String userType) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userType = userType;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return id.equals(user.id) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && userType.equals(user.userType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, userType);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userType='" + userType + '\'' +
+                '}';
+    }
+}
