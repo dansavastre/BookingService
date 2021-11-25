10b1a81ce6c409611c98c01c9d02fcb709b6c064
Luuk Wubben <l.t.wubben@student.tudelft.nl>
2021-11-25 15:08:12 +0100

    Possible fix main_gateway issue

--- main_gateway/src/main/java/nl/tudelft/sem/template/Controllers/UserController.java
+++ main_gateway/src/main/java/nl/tudelft/sem/template/Controllers/UserController.java
@@ -1,8 +1,12 @@
 package nl.tudelft.sem.template.Controllers;
 
+import nl.tudelft.sem.template.Objects.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -14,4 +18,13 @@ public class UserController {
         String result = template.getForObject(uri, String.class);
         return result;
     }
+
+//    @RequestMapping("/getUser")
+//    public String getUser(){
+//        String uri = "http://localhost:8081/getUser";
+//        RestTemplate template = new RestTemplate();
+//        User[] result = ((template.getForObject(uri, User[].class)));
+//        assert result != null;
+//        return Arrays.toString(result);
+//    }
 }
