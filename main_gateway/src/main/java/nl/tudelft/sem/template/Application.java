cbf44d14e231abab0cd40d7b440fbd8a3338a948
vmadhu <v.madhu@student.tudelft.nl>
2021-11-28 16:05:44 +0100

    added dependencies for spring security and postgres driver

--- main_gateway/src/main/java/nl/tudelft/sem/template/Application.java
+++ main_gateway/src/main/java/nl/tudelft/sem/template/Application.java
@@ -17,8 +17,6 @@ import java.util.Scanner;
 public class Application {
 
 	public static void main(String[] args) {
-//		SpringApplication app = new SpringApplication(Application.class);
-//		app.run(args);
 		SpringApplication.run(Application.class, args);
 	}
 
