a3123f7b6d4a6a169059cfbead16f7a2601dc5e9
Luuk Wubben <l.t.wubben@student.tudelft.nl>
2021-12-02 12:37:59 +0100

    Apply Checkstyle and PMD fixes

--- users_microservice/src/main/java/nl/tudelft/sem/template/controllers/UserController.java
+++ users_microservice/src/main/java/nl/tudelft/sem/template/controllers/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    private final UserService userService;
+    private transient UserService userService;
 
     @Autowired
     public UserController(UserService userService) {
