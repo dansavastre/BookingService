873359542d9d3afa84ed8c44eec373589925b6dd
vmadhu <v.madhu@student.tudelft.nl>
2021-12-02 11:11:21 +0100

    initial security setup (user microservice)

---
+++ users_microservice/src/main/java/nl/tudelft/sem/template/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package nl.tudelft.sem.template.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(
+        UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+    
+}
