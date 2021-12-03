1792f36ade7157306cfb607ef70efd7c1f8064c2
Dan <D.Savastre@student.tudelft.nl>
2021-12-03 03:53:33 +0100

    Add tests for Building object

---
+++ main_gateway/src/test/java/object/test/BuildingTest.java
@@ -0,0 +1,39 @@
+package object.test;
+
+import nl.tudelft.sem.template.objects.Building;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+
+public class BuildingTest {
+
+    private transient Building building;
+
+    @BeforeEach
+    void setup() {
+        building = new Building(1, LocalTime.of(8, 0), LocalTime.of(22, 0), "Ewi");
+    }
+
+    @Test
+    void getId_test() {
+        Assertions.assertThat(building.getId()).isEqualTo(1);
+    }
+
+    @Test
+    void getOpeningTime_test() {
+        Assertions.assertThat(building.getOpeningTime()).isEqualTo(LocalTime.of(8, 0));
+    }
+
+    @Test
+    void getClosingTime_test() {
+        Assertions.assertThat(building.getClosingTime()).isEqualTo(LocalTime.of(22, 0));
+    }
+
+    @Test
+    void getName_test() {
+        Assertions.assertThat(building.getName()).isEqualTo("Ewi");
+    }
+
+}
