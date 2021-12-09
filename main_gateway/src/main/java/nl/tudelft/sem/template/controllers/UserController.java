1e7543b587d3d9b3958faee24a0d29c7e55df041
Natalia Pietnoczko <n.pietnoczko@student.tudelft.nl>
2021-12-09 19:30:53 +0100

    Log in from the main gateway

--- main_gateway/src/main/java/nl/tudelft/sem/template/controllers/UserController.java
+++ main_gateway/src/main/java/nl/tudelft/sem/template/controllers/UserController.java
@@ -1,7 +1,13 @@
 package nl.tudelft.sem.template.controllers;
 
 import java.util.List;
+import java.util.Map;
 import nl.tudelft.sem.template.objects.User;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +15,45 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class UserController {
 
     private transient RestTemplate restTemplate = new RestTemplate();
+
+
+    /** Sends the request to user for authentication of the user.
+     *
+     * @param password      encrypted user password
+     * @param username      username of the user
+     * @return Map of tokens received form the user microservice
+     */
+    @PostMapping("/login")
+    @ResponseBody
+    public Map loginUser(@RequestHeader("Authentication") String password,
+                                         @RequestHeader("Username") String username) {
+        String uri = "http://localhost:8081/login";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authentication", password);
+        headers.add("Username", username);
+        HttpEntity<String> entity = new HttpEntity<>("body", headers);
+
+        try {
+            ResponseEntity<Map> res = restTemplate
+                    .exchange(uri, HttpMethod.POST, entity, Map.class);
+            return res.getBody();
+        } catch (HttpClientErrorException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "");
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
     /** Returns all the users in the system.
      *
