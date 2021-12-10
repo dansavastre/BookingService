c3533410f4b0c16a93d1d6487757d72b4db63906
KeshavN <k.nair@student.tudelft.nl>
2021-12-10 12:42:08 +0100

    checkstyle

--- bookings_microservice/src/test/java/schedule/ChronologicalSortTest.java
+++ bookings_microservice/src/test/java/schedule/ChronologicalSortTest.java
@@ -1,13 +1,12 @@
 package schedule;
 
-import nl.tudelft.sem.template.objects.Booking;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import nl.tudelft.sem.template.objects.Booking;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class ChronologicalSortTest {
 
@@ -25,16 +24,16 @@ public class ChronologicalSortTest {
         p.add("user1");
         b1 = new Booking("A", 1, 36, LocalDate.of(2020, 1, 8),
                 LocalTime.of(10, 45, 0), LocalTime.of(12, 45, 0),
-                "Studying",p);
+                "Studying", p);
         b2 = new Booking("B", 3, 36, LocalDate.of(2021, 8, 8),
                 LocalTime.of(10, 45, 0), LocalTime.of(12, 45, 0),
-                "Studying",p);
+                "Studying", p);
         b3 = new Booking("C", 1, 42, LocalDate.of(2020, 4, 8),
                 LocalTime.of(10, 45, 0), LocalTime.of(12, 45, 0),
-                "Studying",p);
+                "Studying", p);
         b4 = new Booking("D", 3, 42, LocalDate.of(2020, 1, 8),
                 LocalTime.of(18, 45, 0), LocalTime.of(12, 45, 0),
-                "Studying",p);
+                "Studying", p);
     }
 
     @Test
