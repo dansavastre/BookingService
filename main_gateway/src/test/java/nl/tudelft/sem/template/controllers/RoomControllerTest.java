76a1adeba74a4197609897e60bf31b8d3c11f427
KeshavN <k.nair@student.tudelft.nl>
2021-12-24 14:46:32 +0100

    Implemented the search feature

--- main_gateway/src/test/java/nl/tudelft/sem/template/controllers/RoomControllerTest.java
+++ main_gateway/src/test/java/nl/tudelft/sem/template/controllers/RoomControllerTest.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import nl.tudelft.sem.template.objects.Building;
 import nl.tudelft.sem.template.objects.Room;
 import org.assertj.core.api.Assertions;
