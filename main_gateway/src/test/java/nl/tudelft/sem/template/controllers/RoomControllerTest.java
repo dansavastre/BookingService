6038f511da203abe35aec9fa0a1358494e20e485
KeshavN <k.nair@student.tudelft.nl>
2021-12-25 17:27:47 +0100

    unfinished testing

--- main_gateway/src/test/java/nl/tudelft/sem/template/controllers/RoomControllerTest.java
+++ main_gateway/src/test/java/nl/tudelft/sem/template/controllers/RoomControllerTest.java
@@ -6,11 +6,17 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import nl.tudelft.sem.template.objects.Booking;
 import nl.tudelft.sem.template.objects.Building;
 import nl.tudelft.sem.template.objects.Room;
 import org.assertj.core.api.Assertions;
@@ -20,6 +26,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -39,15 +46,26 @@ public class RoomControllerTest {
     @InjectMocks
     private transient RoomController roomController;
 
+    @InjectMocks
+    private transient BookingController bookingController;
+
     private transient Room room1;
     private transient Room room2;
     private transient Building building;
     private final transient String token = "token";
     private transient List<Room> rooms;
+    private transient Booking b1;
+    private transient Booking b2;
+    private transient Booking b3;
+    private transient List<Booking> bookings;
+
+    private transient ObjectMapper objectMapper;
 
     @BeforeEach
     void setup() {
         MockitoAnnotations.initMocks(this);
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
         rooms = new ArrayList<>();
         Map<String, String> equipmentMap = new HashMap<>();
         equipmentMap.put("wifi", "True");
@@ -59,6 +77,26 @@ public class RoomControllerTest {
         room2 = new Room(1, "Marie Currie Room", 8, equipmentMap, "available", building);
         rooms.add(room1);
         rooms.add(room2);
+        b1 = new Booking(1L, "Mike", 1, 1,
+                LocalDate.now(),
+                LocalTime.now().plusHours(1),
+                LocalTime.now().plusHours(3),
+                "Group study session",
+                List.of("user0", "user1"));
+        b2 = new Booking(2L, "Mike", 1, 1,
+                LocalDate.now().plusDays(3),
+                LocalTime.now().plusHours(1),
+                LocalTime.now().plusHours(2),
+                "Project room",
+                List.of("user2", "user3"));
+
+        b3 = new Booking(2L, "Mike", 1, 36,
+                LocalDate.of(2021, 12, 31),
+                LocalTime.of(9, 30),
+                LocalTime.of(12, 0),
+                "Project room",
+                List.of("user2", "user3"));
+        bookings = new ArrayList<>(List.of(b1, b2,b3));
     }
 
     @Test
@@ -123,5 +161,16 @@ public class RoomControllerTest {
         verify(restTemplate, times(1)).exchange(eq(uri), eq(HttpMethod.DELETE),
                 entity.capture(), eq(void.class));
         assertEquals(token, entity.getValue().getHeaders().getFirst(HttpHeaders.AUTHORIZATION));
+    }
+
+    @Test
+    void availableRoom_test() {
+        RoomController spyRoomController = Mockito.spy(roomController);
+        List<Room> result = new ArrayList<>();
+        result.add(room1);
+        result.add(room2);
+        Mockito.doReturn(rooms).when(spyRoomController).getRooms(token);
+        when(bookingController.getFutureBookings(token)).thenReturn(bookings);
+        assertEquals(roomController.availableRooms("2022-12-31", "10:00:00", "18:30:00", token),result);
     }
 }
