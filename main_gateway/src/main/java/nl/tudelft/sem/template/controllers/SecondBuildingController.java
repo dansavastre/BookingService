88d1ec0650f40c973b50f9d0e1b566677c5d106c
KeshavN <k.nair@student.tudelft.nl>
2022-01-15 13:37:19 +0100

    Fixed high coupling in building controller

--- main_gateway/src/main/java/nl/tudelft/sem/template/controllers/SecondBuildingController.java
+++ main_gateway/src/main/java/nl/tudelft/sem/template/controllers/SecondBuildingController.java
@@ -1,15 +1,12 @@
 package nl.tudelft.sem.template.controllers;
 
-import java.util.List;
 import nl.tudelft.sem.template.objects.Building;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,30 +19,33 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 
-@Controller
-public class BuildingController {
+public class SecondBuildingController {
 
     @Autowired
     private transient RestTemplate restTemplate;
 
-    /** Returns all the buildings in the system.
+
+    /** Returns a specific building with respect to its id.
      *
+     * @param id        the id of the building we want.
      * @param token     the token of the user
-     * @return list of the buildings.
+     * @return the building we are searching for.
      */
-    @GetMapping("/getBuildings")
+    @GetMapping("/getBuilding/{id}")
     @ResponseBody
-    public List getBuildings(@RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
-        String uri = "http://localhost:8082/buildings";
+    public Building getBuilding(@PathVariable("id") int id,
+                                @RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+        String uri = "http://localhost:8082/getBuilding/".concat(String.valueOf(id));
 
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.AUTHORIZATION, token);
         HttpEntity<String> entity = new HttpEntity<>("body", headers);
 
         try {
-            ResponseEntity<List> res = restTemplate
-                    .exchange(uri, HttpMethod.GET, entity, List.class);
+            ResponseEntity<Building> res = restTemplate
+                    .exchange(uri, HttpMethod.GET, entity, Building.class);
             return res.getBody();
         } catch (HttpClientErrorException e) {
             throw new ResponseStatusException(e.getStatusCode(), e.toString());
@@ -53,27 +53,24 @@ public class BuildingController {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "");
         }
     }
-
-    /** Update a building.
+    /** Adds a building to the system.
      *
-     * @param building the new building.
-     * @param id the id of the building to update.
      * @param token     the token of the user
-     * @return true if successfully updated, else false.
+     * @param building the building we want to add.
+     * @return true if its successfully added, else false.
      */
-    @PutMapping("/putBuilding/{id}")
+    @PostMapping("/postBuilding")
     @ResponseBody
-    public boolean updateBuilding(@RequestBody Building building,
-                                  @PathVariable("id") int id,
-                                  @RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
-        String uri = "http://localhost:8082/buildings/".concat(String.valueOf(id));
+    public boolean postBuilding(@RequestBody Building building,
+                                @RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+        String uri = "http://localhost:8082/buildings";
 
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.AUTHORIZATION, token);
         HttpEntity<Building> entity = new HttpEntity<>(building, headers);
 
         try {
-            restTemplate.exchange(uri, HttpMethod.PUT, entity, void.class);
+            restTemplate.exchange(uri, HttpMethod.POST, entity, void.class);
             return true;
         } catch (HttpClientErrorException e) {
             throw new ResponseStatusException(e.getStatusCode(), e.toString());
@@ -82,29 +79,6 @@ public class BuildingController {
         }
     }
 
-    /** Deletes a building from the system.
-     *
-     * @param id the id of the building to delete.
-     * @param token     the token of the user
-     * @return true if successfully deleted, else false.
-     */
-    @DeleteMapping("/deleteBuilding/{id}")
-    @ResponseBody
-    public boolean deleteBuilding(@PathVariable("id") int id,
-                                  @RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
-        String uri = "http://localhost:8082/buildings/".concat(String.valueOf(id));
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.AUTHORIZATION, token);
-        HttpEntity<String> entity = new HttpEntity<>("body", headers);
-        try {
-            restTemplate.exchange(uri, HttpMethod.DELETE, entity, void.class);
-            return true;
-        } catch (HttpClientErrorException e) {
-            throw new ResponseStatusException(e.getStatusCode(), e.toString());
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "");
-        }
-    }
 
 }
--- rooms_microservice/src/main/java/nl/tudelft/sem/template/controllers/BuildingController.java
+++ rooms_microservice/src/main/java/nl/tudelft/sem/template/controllers/BuildingController.java
@@ -46,38 +46,5 @@ public class BuildingController {
         return buildingService.getBuilding(id);
     }
 
-    @PostMapping("/buildings")
-    @ResponseBody
-    public void addBuilding(@RequestBody Building building,
-                            @RequestHeader(authorization) String token) {
-        auth.authorize(Authorization.ADMIN, token);
-        buildingService.addBuilding(building);
-    }
-
-    /**
-     * Update an existing building.
-     *
-     * @param building The building to be updated with new info
-     * @param id       The ID of the building to be updated
-     * @param token    Security token for authentication
-     */
-    @PutMapping("/buildings/{id}")
-    @ResponseBody
-    public void updateBuilding(@RequestBody Building building, @PathVariable("id") int id,
-                               @RequestHeader(authorization) String token) {
-        auth.authorize(Authorization.ADMIN, token);
-        Building toUpdate = buildingService.getBuilding(id);
-        if (toUpdate.getId() == building.getId()) {
-            buildingService.updateBuilding(id, building);
-        }
-    }
-
-    @DeleteMapping("/buildings/{id}")
-    @ResponseBody
-    public void deleteBuilding(@PathVariable("id") int id,
-                               @RequestHeader(authorization) String token) {
-        auth.authorize(Authorization.ADMIN, token);
-        buildingService.deleteBuilding(id);
-    }
 }
 
