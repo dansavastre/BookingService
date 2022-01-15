245f86327468d16b71bc54fa0df5cec623a87430
Natalia Pietnoczko <n.pietnoczko@student.tudelft.nl>
2022-01-15 13:16:27 +0100

    extract myBookingsController from bookingController in booking ms

---
+++ bookings_microservice/src/main/java/nl/tudelft/sem/template/controllers/MyBookingsController.java
@@ -0,0 +1,79 @@
+package nl.tudelft.sem.template.controllers;
+
+import java.util.List;
+import nl.tudelft.sem.template.objects.Booking;
+import nl.tudelft.sem.template.schedule.ChronologicalSortStrategy;
+import nl.tudelft.sem.template.schedule.DefaultSortStrategy;
+import nl.tudelft.sem.template.schedule.LocationStrategy;
+import nl.tudelft.sem.template.services.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/myBookings")
+public class MyBookingsController {
+
+    @Autowired
+    private transient BookingService bookingService;
+
+    @Autowired
+    private transient Authorization auth;
+
+    private static final String userIdPath = "userId";
+
+    @PutMapping("/{userId}/{id}")
+    @ResponseBody
+    public void updateMyBooking(@RequestBody Booking booking,
+                                @PathVariable(userIdPath) String userId,
+                                @PathVariable("id") Long id,
+                                @RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+        auth.authorizeWithUsername(Authorization.EMPLOYEE, token, userId);
+        bookingService.updateMyBooking(userId, id, booking);
+    }
+
+
+    @DeleteMapping("/{userId}/{id}")
+    @ResponseBody
+    public void deleteBooking(@PathVariable(userIdPath) String userId,
+                              @PathVariable("id") Long id,
+                              @RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+        auth.authorizeWithUsername(Authorization.EMPLOYEE, token, userId);
+        bookingService.deleteMyBooking(userId, id);
+    }
+
+    @GetMapping("/default/{userId}")
+    @ResponseBody
+    public List<Booking> getMyBookingsDefault(@PathVariable(userIdPath) String userId,
+                                              @RequestHeader(HttpHeaders
+                                                      .AUTHORIZATION) String token) {
+        auth.authorizeWithUsername(Authorization.EMPLOYEE, token, userId);
+        return bookingService.getBookingsForUser(userId, new DefaultSortStrategy());
+    }
+
+    @GetMapping("/chrono/{userId}")
+    @ResponseBody
+    public List<Booking> getMyBookingsChrono(@PathVariable(userIdPath) String userId,
+                                             @RequestHeader(HttpHeaders
+                                                     .AUTHORIZATION) String token) {
+        auth.authorizeWithUsername(Authorization.EMPLOYEE, token, userId);
+        return bookingService.getBookingsForUser(userId, new ChronologicalSortStrategy());
+    }
+
+    @GetMapping("/location/{userId}")
+    @ResponseBody
+    public List<Booking> getMyBookingsLocation(@PathVariable(userIdPath) String userId,
+                                               @RequestHeader(HttpHeaders
+                                                       .AUTHORIZATION) String token) {
+        auth.authorizeWithUsername(Authorization.EMPLOYEE, token, userId);
+        return bookingService.getBookingsForUser(userId, new LocationStrategy());
+    }
+}
