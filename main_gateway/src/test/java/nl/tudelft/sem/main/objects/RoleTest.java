3497456244ec93562268a8a62649eca9646e4ff0
KeshavN <k.nair@student.tudelft.nl>
2022-01-16 22:43:03 +0100

    testing for role.java to make line and mutation coverage 100%

--- main_gateway/src/test/java/nl/tudelft/sem/main/objects/RoleTest.java
+++ main_gateway/src/test/java/nl/tudelft/sem/main/objects/RoleTest.java
@@ -4,6 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class RoleTest {
 
     private transient Role role;
@@ -30,5 +33,48 @@ public class RoleTest {
         Assertions.assertThat(role.getType()).isEqualTo("admin");
     }
 
+    @Test
+    void setId_test() {
+        role.setId(3l);
+        Assertions.assertThat(role.getId()).isEqualTo(3L);
+    }
+
+    @Test
+    void equalsTrue_test() {
+        Role role2 = new Role();
+        role2.setType("employee");
+        role2.setId(1L);
+        assertTrue(role.equals(role2));
+        assertTrue(role.hashCode() == role2.hashCode());
+    }
+
+    @Test
+    void equalsSame_test() {
+        assertTrue(role.equals(role));
+        assertTrue(role.hashCode() == role.hashCode());
+    }
+
+    @Test
+    void equalsFalse_test() {
+        Role role2 = new Role("employee");
+        role2.setId(2L);
+        role2.setType("admin");
+        assertFalse(role.equals(role2));
+        assertFalse(role.hashCode() == role2.hashCode());
+    }
+
+    @Test
+    void equalsNull_test() {
+        assertFalse(role.equals(null));
+    }
+    @Test
+    void equalsWrongClass_test() {
+        assertFalse(role.equals("1"));
+    }
+
+    @Test
+    void toString_test() {
+        Assertions.assertThat(role.toString()).isEqualTo("Role{id='1', type='employee'}");
+    }
 
 }
